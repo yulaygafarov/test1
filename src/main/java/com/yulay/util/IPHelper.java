e1e2e65a786b261b422c3413f05ff7a2afa67a00
yulay <yulay@YulayPC.lan>
2019-07-11 21:31:06 +0500

    Added real tests

--- src/main/java/com/yulay/util/IPHelper.java
+++ src/main/java/com/yulay/util/IPHelper.java
@@ -1,5 +1,12 @@
 package com.yulay.util;
 
 public class IPHelper {
+	public static int ipToInt(String ip) {
+		return 0;
+	}
+
+	public static String intToIp(int ipIntValue) {
+		return null;
+	}
 
 }
--- src/test/java/com/yulay/util/IPHelperTest.java
+++ src/test/java/com/yulay/util/IPHelperTest.java
@@ -1,11 +1,55 @@
 package com.yulay.util;
 
-public class IPHelperTest {
-	public void testIpToInt() {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class IPHelperTest {
+	@Test
+	public void testIntToIpWhenZero() {
+		int IpIntValue = 0;
+		String expected = "0.0.0.0";
+		String actual = IPHelper.intToIp(IpIntValue);
+		assertEquals(expected, actual);
 	}
 
-	public void testIntToIp() {
+	@Test
+	public void testIntToIpWhenRegularValue() {
+		int IpIntValue = 389417295;
+		String expected = "23.54.9.79";
+		String actual = IPHelper.intToIp(IpIntValue);
+		assertEquals(expected, actual);
+	}
 
+	@Test
+	public void testIntToIpWhenMaxValue() {
+		int IpIntValue = 0xFF_FF_FF_FF;
+		String expected = "255.255.255.255";
+		String actual = IPHelper.intToIp(IpIntValue);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testIpToIntWhenZero() {
+		String ip = "0.0.0.0";
+		int expected = 0;
+		int actual = IPHelper.ipToInt(ip);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testIpToIntWhenRegularValue() {
+		String ip = "23.54.9.79";
+		int expected = 389417295;
+		int actual = IPHelper.ipToInt(ip);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testIpToIntWhenMaxValue() {
+		String ip = "255.255.255.255";
+		int expected = 0xFF_FF_FF_FF;
+		int actual = IPHelper.ipToInt(ip);
+		assertEquals(expected, actual);
 	}
 }
